f78069a23b2cf76feff5cb2a525d8bf406f058ef
Joabson de Souza <joabsondesouza@hotmal.com>
2021-06-18 09:27:27 -0300

    PushNotification

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        React Native Push Notification Programadorzao
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
